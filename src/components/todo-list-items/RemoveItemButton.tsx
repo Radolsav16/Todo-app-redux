eeaa62d2f863e4254955ac88cd4e1b9e498d65c6
Radoslav Todorov <todorovradoslav18@gmail.com>
2025-07-13 07:54:57 +0300

    make reomove todo functionality

--- src/components/todo-list-items/RemoveItemButton.tsx
+++ src/components/todo-list-items/RemoveItemButton.tsx
@@ -1,8 +1,19 @@
 import { ReactElement } from "react";
+import { useAppDispatch} from "../../app/hooks";
+import { deleteTodo } from "../../features/todos/todosThunks";
 
-const RemoveItemButton:React.FC = ():ReactElement =>{
+type Props =  {
+    id:string
+}
+
+const RemoveItemButton:React.FC<Props> = ({id}):ReactElement =>{
+    const dispatch = useAppDispatch()
+
+    const deleteHanlder = () =>{
+        dispatch(deleteTodo(id))
+    }
     return (
-         <a className="remove-btn" title="Remove item">
+         <a className="remove-btn" title="Remove item" onClick={deleteHanlder}>
                   <i className="fas fa-times text-primary" />
          </a>
     )
